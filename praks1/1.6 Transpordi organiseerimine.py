dbb319d24385a3c7948eeaf88072c9fc8b0dff81
Glywnok <arturlohatin@gmail.com>
2021-10-14 11:00:59 +0300

    Tegin faili lõppuni, kontrollisin ja ta töötab

--- praks1/1.6 Transpordi organiseerimine.py
+++ praks1/1.6 Transpordi organiseerimine.py
@@ -1,25 +1,27 @@
-x = input("Sisestage mitu inimest teil on: ")
+x = int(input("Sisestage mitu inimest teil on: "))
 
-y = input("Sisestage mitu bussi teil on: ")
+y = int(input("Sisestage mitu bussi teil on: "))
 
-z = input("Sisestage mitu inimest kokku võib istuda bussil ilma bussijuht: ")
+z = int(input("Sisestage mitu inimest kokku võib istuda bussil ilma bussijuht: "))
 
 #x on inimest
 #y on bussi
-#z on inimest ühes bussis.
-#a on mitu inimest saab bussi
-#b on kokku mitu inimest jääb ära, võib teha if-iga asju...
+#z on inimest ühes bussis võib istuda(ilma bussijuthti).
+#mitu_kohhti on mitu on kokku kohti
+#saab_sisse on mitu inimest saab bussi
+#c on kokku mitu inimest jääb ära, võib teha if-iga asju...
 
 #siin kirjutan arvutuse
 
-mitu_kohti = y  z
-c = x / mitu_kohti
-
-#redaktuur 10:18 PM halvasti on...
+mitu_kohti = ( y * z )
+c = ( x % mitu_kohti )
+saab_sisse = ( mitu_kohti - c )
+#redaktuur 10:18 PM kodus 09.10: halvasti on...
+#redaktuur 10:41 AM tunnis 14.10: hästi tuleb
 
 #siin ma printid mitu inmest on kokku
 
-if b > 0
-print("Kokku on " + x + " inimest ja " + y + " bussi. Kokku ühes bussis võib istuda " + z + "inimest. Kokku bussi saab " + a + "inimest ilma bussijuthi ja " + b + " inimest saab ära jääda."
-      else
-      print("Kokku on " + x + " inimest ja " + y + " bussi. Kokku ühes bussis võib istuda " + z + "inimest. Kokku bussi saab " + a + "inimest ilma bussijuthi ja mitte keegi ei jää ära."
+if c > 0:
+    print("Kokku on " + str(x) + " inimest ja " + str(y) + " bussi. Kokku ühes bussis võib istuda " + str(z) + " inimest. Kokku bussi saab " + str(saab_sisse) + "inimest ilma bussijuthi ja " + str(c) + " inimest saab ära jääda.")
+else:
+    print("Kokku on " + str(x) + " inimest ja " + str(y) + " bussi. Kokku ühes bussis võib istuda " + str(z) + " inimest. Kokku bussi saab " + str(saab_sisse) + "inimest ilma bussijuthi ja mitte keegi ei jää ära!")
