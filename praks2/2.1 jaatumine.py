35ab7ce9212bdaec3603ef4acce7e2bc2fce10ff
Glywnok <arturlohatin@gmail.com>
2021-10-19 09:48:00 +0300

    Alustasime 2 paragraphi

---
+++ praks2/2.1 jaatumine.py
@@ -0,0 +1,7 @@
+Temperatuur = int(input("Mis temperatuur on õues? "))
+if Temperatuur > 0:
+    print("Õues on " + str(Temperatuur) + " kraadi Celsiumis.")
+    print("Pole jäätmise ohu.")
+else:
+    print("Õues on " + str(Temperatuur) + " kraadi Celsiumis.")
+    print("On jäätmise oht.")
