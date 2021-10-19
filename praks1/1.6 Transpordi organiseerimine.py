a4ce50bc7336471b0703c82e57aadc0170c07385
Glywnok <arturlohatin@gmail.com>
2021-10-19 09:44:27 +0300

    Tegin lõppuni 1.6 klassis

--- praks1/1.6 Transpordi organiseerimine.py
+++ praks1/1.6 Transpordi organiseerimine.py
@@ -1,8 +1,8 @@
-x = int(input("Sisestage mitu inimest teil on: "))
+#x = int(input("Sisestage mitu inimest teil on: "))
 
-y = int(input("Sisestage mitu bussi teil on: "))
+#y = int(input("Sisestage mitu bussi teil on: "))
 
-z = int(input("Sisestage mitu inimest kokku võib istuda bussis: "))
+#z = int(input("Sisestage mitu inimest kokku võib istuda bussis: "))
 
 #x on inimest
 #y on bussi
@@ -13,15 +13,36 @@ z = int(input("Sisestage mitu inimest kokku võib istuda bussis: "))
 
 #siin kirjutan arvutuse
 
-mitu_kohti = ( y * z )
-c = ( x - mitu_kohti )
-saab_sisse = ( mitu_kohti - c )
+#mitu_kohti = ( y * z )
+#c = ( x - mitu_kohti )
+#saab_sisse = ( mitu_kohti - c )
+
+#saab_sisse1 = x % z
+#saab_sisse2 = x - saab_sisse1
+#saab_sisse3 = saab_sisse2 / z
+
+#print(saab_sisse3)
+
 #redaktuur 10:18 PM kodus 09.10: halvasti on...
 #redaktuur 10:41 AM tunnis 14.10: hästi tuleb
 
 #siin ma printid mitu inmest on kokku
 
-if c > 0:
-    print("Kokku on " + str(x) + " inimest ja " + str(y) + " bussi. Kokku ühes bussis võib istuda " + str(z) + " inimest. Kokku bussi saab " + str(saab_sisse) + " inimest,  ja " + str(c) + " inimest saab ära jääda.")
+#if c > 0:
+    #print("Kokku on " + str(x) + " inimest ja " + str(y) + " bussi. Kokku ühes bussis võib istuda " + str(z) + " inimest. Kokku bussi saab " + str(saab_sisse) + " inimest,  ja " + str(c) + " inimest saab ära jääda.")
+#else:
+    #print("Kokku on " + str(x) + " inimest ja " + str(y) + " bussi. Kokku ühes bussis võib istuda " + str(z) + " inimest. Kokku bussi saab " + str(saab_sisse) + " inimest, ja mitte keegi ei jää ära!")
+
+
+
+#Tegime klassis
+
+inimest = int(input("Sisesta inimeste arv "))
+kohti = int(input("Sisesta mitu kohti on ühes bussis "))
+bussid = float(inimest) / kohti
+mahajaanud = inimest % kohti
+if mahajaanud > 0:
+    bussid = bussid + 1
+    print("Kokku on " + str(inimest) + " inimest. Kohti ühes bussis on " + str(kohti) + ". On vaja tellida " + str(round(bussid)) + " bussi, sest on mahajääänud " + str(mahajaanud) + " inimest.")
 else:
-    print("Kokku on " + str(x) + " inimest ja " + str(y) + " bussi. Kokku ühes bussis võib istuda " + str(z) + " inimest. Kokku bussi saab " + str(saab_sisse) + " inimest, ja mitte keegi ei jää ära!")
+    print("Kokku on " + str(inimest) + " inimest. Kohti ühes bussis on " + str(kohti) + ". On vaja tellida " + str(round(bussid)) + " bussi, ja mitte keegi ei ole mahajäänud.")
