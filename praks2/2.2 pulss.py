9e7948a877ea51c186cc6891a63a660cace04507
Glywnok <arturlohatin@gmail.com>
2021-10-27 12:05:08 +0300

    lihsalt ümber tegin, veel ei tööta

--- praks2/2.2 pulss.py
+++ praks2/2.2 pulss.py
@@ -1,19 +1,22 @@
-vanus = int(input("Sisestage oma vanus "))
-sugu = input("Sisestage oma sugu ")
-Trenningtype = input("Sisestage oma trenningu tüüp: 1 - tervisetreening, 2 - põhivastupidavuse treening, 3 - intensiivne aeroobne treening. ")
+print("Sisestage oma vanus")
+vanus = int(input())
+print("Sisestage oma sugu")
+sugu = input()
+print("Sisestage oma trenningu tüüp: 1 - tervisetreening, 2 - põhivastupidavuse treening, 3 - intensiivne aeroobne treening.   ")
+Trenningtype = input()
 if sugu == "M" or sugu == "m":
     puls = 220 - vanus
 if sugu == "N" or sugu == "n":
     puls = 206 - vanus * 0.88
-pulssmadal = 0
-pulsskorg = 0
 if Trenningtype == 1:
     pulssmadal = 0.5 * puls
     pulsskorg = 0.7 * puls
-elif Trenningtype == 2:
-    pulssmadal = 0.7 * puls
-    pulsskorg = 0.8 * puls
-elif Trenningtype == 3:
-    pulssmadal = 0.8 * puls
-    pulsskorg = 0.87 * puls
+else:
+    if Trenningtype == 2:
+        pulssmadal = 0.7 * puls
+        pulsskorg = 0.8 * puls
+    else:
+        if Trenningtype == 3:
+            pulssmadal = 0.8 * puls
+            pulsskorg = 0.87 * puls
 print("Pulsisagedus peaks olema vahemikus " + str(pulssmadal) + " ja " + str(pulsskorg) + ".")
